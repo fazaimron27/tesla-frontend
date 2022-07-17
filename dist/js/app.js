4a542b77b2aa7a3ff882e678baede3bbb2de1da7
fazaimron27 <cincaumyself@gmail.com>
2022-07-17 19:33:50 +0700

    rebuild for production

---
+++ dist/js/app.js
@@ -0,0 +1,49 @@
+const host_endpoint =
+  "https://tesla-backend-rest-server-production.up.railway.app/api";
+
+axios
+  .get(`${host_endpoint}/teams`)
+  .then((response) => {
+    // console.log(response.data);
+    response.data.forEach(function (val) {
+      // console.log(val);
+      $("#teams").append(`
+        <div class="Team w-40">
+          <img src="${
+            val.avatar || "dist/image/team/def.png"
+          }" class="border-8 border-white rounded-full h-auto w-36 mx-auto" />
+          <div class="m-4 flex flex-col justify-center">
+            <span class="font-bold font-sans text-white text-center">${
+              val.role
+            }</span>
+            <span class="block font-sans text-white text-sm text-center">${
+              val.name
+            }</span>
+            <span class="block font-sans text-white text-sm text-center">${
+              val.id_card
+            }</span>
+          </div>
+        </div>
+      `);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+$("#send").on("click", () => {
+  const data = {
+    name: $("#name").val(),
+    email: $("#email").val(),
+    message: $("#message").val(),
+  };
+  axios
+    .post(`${host_endpoint}/messages`, data)
+    .then((response) => {
+      // console.log(response.data);
+      alert("Pesan Terkirim");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
